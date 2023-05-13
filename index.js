c3f7a200d599e570a79719b8d86501275d75d7d9
Irvin Dinkins <irvdude44@gmail.com>
2023-05-13 17:12:36 -0400

    partially passed

--- index.js
+++ index.js
@@ -9,7 +9,7 @@
  for you to use if you need it!
  */
 
-const allWagesFor = function () {
+ const allWagesFor = function () {
     const eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
     })
@@ -21,3 +21,79 @@ const allWagesFor = function () {
     return payable
 }
 
+function createEmployeeRecord(array) {
+    const employeeRecords = {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+    return employeeRecords
+}
+
+
+function createEmployeeRecords(employeeData) {
+    let employeeRecords = [];
+
+    employeeData.forEach(function (array) {
+        let employeeRecord = {
+            firstName: array[0],
+            lastName: array[1],
+            employeeId: array[2],
+            timeInEvents: [],
+            timeOutEvents: []
+        };
+        employeeRecords.push(employeeRecord);
+    });
+
+    return employeeRecords;
+}
+
+function createTimeInEvent(employeeRecord, timestamp) {
+    let timeInEvent = {
+        type: "TimeIn",
+        date: timestamp.split(" ")[0],
+        hour: parseInt(timestamp.split(" ")[1])
+    };
+
+    employeeRecord.timeInEvents.push(timeInEvent);
+
+    return employeeRecord;
+}
+
+function createTimeOutEvent(employeeRecord, timestamp) {
+    let timeOutEvent = {
+        type: "TimeOut",
+        date: timestamp.split(" ")[0],
+        hour: parseInt(timestamp.split(" ")[1])
+    };
+
+    employeeRecord.timeOutEvents.push(timeOutEvent);
+
+    return employeeRecord;
+}
+
+function hoursWorkedOnDate(employeeRecord, date) {
+    const timeInEvent = employeeRecord.timeInEvents.find(function (e) {
+        return e.date === date
+    })
+
+    const timeOutEvent = employeeRecord.timeOutEvents.find(function (e) {
+        return e.date === date
+    })
+
+    const hoursWorked = (timeOutEvent.hour - timeInEvent.hour) / 100
+
+    return hoursWorked
+
+}
+
+function wagesEarnedOnDate(employeeRecord, date) {
+    const hoursWorked = hoursWorkedOnDate(employeeRecord, date)
+    const hourlyWage = employeeRecord.payPerHour
+    const wagesEarned = hoursWorked * hourlyWage
+
+    return wagesEarned
+}
